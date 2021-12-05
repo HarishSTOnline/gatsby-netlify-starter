a34eb14d76d5968ed2f106e2e8bb84bfacd59628
HarishST <harishstprojects@gmail.com>
2021-12-05 20:26:02 +0530

    Default blogs deleted. Added a new blog post.

---
+++ content/blog/first-post/index.md
@@ -0,0 +1,9 @@
+---
+title: MOVE WEBROOT TO A NEW LOCATION (EXTERNAL DISK)
+date: "2021-12-05T14:48:10Z"
+description: "Beginning of wonderful things."
+---
+
+
+- [Change Nginx config](https://www.digitalocean.com/community/tutorials/how-to-move-an-nginx-web-root-to-a-new-location-on-ubuntu-18-04).
+- [Add Execute permission to the new web root](https://stackoverflow.com/questions/25774999/nginx-stat-failed-13-permission-denied).
--- src/components/bio.js
+++ src/components/bio.js
@@ -31,28 +31,32 @@ const Bio = () => {
   const social = data.site.siteMetadata?.social
 
   return (
-    <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
-      {author?.name && (
-        <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
-        </p>
-      )}
-    </div>
+    <div></div>
   )
+
+  // return (
+  //   <div className="bio">
+  //     <StaticImage
+  //       className="bio-avatar"
+  //       layout="fixed"
+  //       formats={["auto", "webp", "avif"]}
+  //       src="../images/profile-pic.png"
+  //       width={50}
+  //       height={50}
+  //       quality={95}
+  //       alt="Profile picture"
+  //     />
+  //     {author?.name && (
+  //       <p>
+  //         Written by <strong>{author.name}</strong> {author?.summary || null}
+  //         {` `}
+  //         <a href={`https://twitter.com/${social?.twitter || ``}`}>
+  //           You should follow them on Twitter
+  //         </a>
+  //       </p>
+  //     )}
+  //   </div>
+  // )
 }
 
 export default Bio
